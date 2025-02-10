8e59fbe850ef5b7957a5568e2366887efc9aecac
JM Mac <johnmarkmancol@gmail.com>
2025-02-10 15:49:01 +0800

    feat: tokenomics section

--- src/components/sections/tokenomics.tsx
+++ src/components/sections/tokenomics.tsx
@@ -9,10 +9,184 @@ export function TokenomicsSection() {
         <h2 className="text-[2.3em] sm:text-[3em] mb-4 sm:mb-6 text-accent">
           Tokenomics
         </h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor adipisci
-        laudantium, esse ad sapiente quibusdam eveniet accusantium veniam iure
-        magnam voluptatem debitis, quae quisquam temporibus voluptas vel unde
-        quod eos!
+        <div className="flex gap-4">
+          <div className="w-1/9">
+            <div className="flex items-center gap-2 text-accent">
+              <img
+                src="/images/social/file-yellow.png"
+                alt=""
+                className="h-6"
+              />
+              $MIMING Whitepaper
+            </div>
+          </div>
+          <div className="flex-1">
+            <div className="relative">
+              <img
+                src="/images/miming-logo.png"
+                className="h-auto w-[20%] absolute right-[28%] top-[40%]"
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100%"
+                height="auto"
+                viewBox="0 0 1085.17 644.29"
+              >
+                <g transform="translate(389.14, 0)">
+                  <g></g>
+                </g>
+                <g transform="translate(677.14, 356.29)">
+                  <g>
+                    <g>
+                      <g pointerEvents="auto">
+                        <g style={{ cursor: "pointer", outline: "none" }}>
+                          <path
+                            d="M0 -288 A288 288 0 1 1 -169.28 232.99 L0 0 Z"
+                            fill="#5c0e74"
+                            opacity="1"
+                          />
+                        </g>
+                      </g>
+                      <g pointerEvents="auto">
+                        <g style={{ cursor: "pointer", outline: "none" }}>
+                          <path
+                            d="M-169.28 232.99 A288 288 0 0 1 -273.90 -88.99 L0 0 Z"
+                            fill="#5f9ab4"
+                            opacity="1"
+                          />
+                        </g>
+                      </g>
+                      <g pointerEvents="auto">
+                        <g style={{ cursor: "pointer", outline: "none" }}>
+                          <path
+                            d="M-273.90 -88.99 A288 288 0 0 1 -169.28 -232.99 L0 0 Z"
+                            fill="#6a024a"
+                            opacity="1"
+                          />
+                        </g>
+                      </g>
+                      <g pointerEvents="auto">
+                        <g style={{ cursor: "pointer", outline: "none" }}>
+                          <path
+                            d="M-169.28 -232.99 A288 288 0 0 1 -0.02 -287.99 L0 0 Z"
+                            fill="#a51c55"
+                            opacity="1"
+                          />
+                        </g>
+                      </g>
+                    </g>
+                    <g>
+                      <text
+                        className="text-white"
+                        textAnchor="middle"
+                        dominantBaseline="alphabetic"
+                        x="347.02"
+                        y="106.85"
+                        opacity="1"
+                        fontSize="22.8"
+                        fontFamily="Arial, sans-serif"
+                        fill="white"
+                      >
+                        Public Sale
+                      </text>
+                      <text
+                        className="text-white"
+                        textAnchor="middle"
+                        dominantBaseline="alphabetic"
+                        x="347.02"
+                        y="135.97"
+                        opacity="1"
+                        fontSize="22.8"
+                        fontFamily="Arial, sans-serif"
+                        fill="white"
+                      >
+                        60%
+                      </text>
+                      <text
+                        className="text-white"
+                        textAnchor="middle"
+                        dominantBaseline="alphabetic"
+                        x="-475.14"
+                        y="145.66"
+                        opacity="1"
+                        fontSize="22.8"
+                        fontFamily="Arial, sans-serif"
+                        fill="white"
+                      >
+                        Initial Liquidity, Community Airdrops
+                      </text>
+                      <text
+                        className="text-white"
+                        textAnchor="middle"
+                        dominantBaseline="alphabetic"
+                        x="-474.64"
+                        y="179.78"
+                        opacity="1"
+                        fontSize="22.8"
+                        fontFamily="Arial, sans-serif"
+                        fill="white"
+                      >
+                        20%
+                      </text>
+                      <text
+                        className="text-white"
+                        textAnchor="middle"
+                        dominantBaseline="alphabetic"
+                        x="-322.88"
+                        y="-243.64"
+                        opacity="1"
+                        fontSize="22.8"
+                        fontFamily="Arial, sans-serif"
+                        fill="white"
+                      >
+                        Development Fund
+                      </text>
+                      <text
+                        className="text-white"
+                        textAnchor="middle"
+                        dominantBaseline="alphabetic"
+                        x="-322.88"
+                        y="-209.52"
+                        opacity="1"
+                        fontSize="22.8"
+                        fontFamily="Arial, sans-serif"
+                        fill="white"
+                      >
+                        10%
+                      </text>
+                      <text
+                        className="text-white"
+                        textAnchor="middle"
+                        dominantBaseline="alphabetic"
+                        x="-106.65"
+                        y="-337.29"
+                        opacity="1"
+                        fontSize="22.8"
+                        fontFamily="Arial, sans-serif"
+                        fill="white"
+                      >
+                        Infrastructure, Marketing and Admin
+                      </text>
+                      <text
+                        className="text-white"
+                        textAnchor="middle"
+                        dominantBaseline="alphabetic"
+                        x="-106.65"
+                        y="-302.17"
+                        opacity="1"
+                        fontSize="22.8"
+                        fontFamily="Arial, sans-serif"
+                        fill="white"
+                      >
+                        10%
+                      </text>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
