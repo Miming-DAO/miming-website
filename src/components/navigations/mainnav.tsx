cd595c049b10fa228bcafecf54fe306ffe855c8e
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-05-23 01:52:29 +0800

    added the launch app button

--- src/components/navigations/mainnav.tsx
+++ src/components/navigations/mainnav.tsx
@@ -8,40 +8,32 @@ export function MainNav() {
   return (
     <ul className="flex items-center gap-4 sm:gap-10 text-sm text-darumadrop sm:text-xl text-white">
       <li>
-        <a
-          href="#"
-          className="font-bold underline uppercase"
-          onClick={() => scrollToSection("our-story")}
-        >
+        <a href="#" className="font-bold underline uppercase" onClick={() => scrollToSection("our-story")}>
           Our Story
         </a>
       </li>
       <li>
-        <a
-          href="#"
-          className="font-bold underline uppercase"
-          onClick={() => scrollToSection("tokenomics")}
-        >
+        <a href="#" className="font-bold underline uppercase" onClick={() => scrollToSection("tokenomics")}>
           Tokenomics
         </a>
       </li>
       <li>
-        <a
-          href="#"
-          className="font-bold underline uppercase"
-          onClick={() => scrollToSection("utility")}
-        >
+        <a href="#" className="font-bold underline uppercase" onClick={() => scrollToSection("utility")}>
           Utility
         </a>
       </li>
       <li>
-        <a
-          href="#"
-          className="font-bold underline uppercase"
-          onClick={() => scrollToSection("roadmap")}
-        >
+        <a href="#" className="font-bold underline uppercase" onClick={() => scrollToSection("roadmap")}>
           Roadmap
         </a>
+      </li>
+      <li>
+        <button
+          onClick={() => window.open("https://dapp.miming.net", "_blank")}
+          className="bg-gradient-to-r from-pink-600 via-pink-700 to-pink-800 text-white font-bold pt-2 pb-3 px-6 rounded-xl shadow-md hover:shadow-lg transition duration-300 ease-in-out uppercase"
+        >
+          Launch App
+        </button>
       </li>
     </ul>
   );
